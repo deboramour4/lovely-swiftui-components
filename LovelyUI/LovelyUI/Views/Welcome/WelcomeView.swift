4233a0d0c12dc760a442918cebf7484b243c600e
Debora Moura <deboramour4@gmail.com>
2020-07-05 18:28:48 -0300

    Create Welcome view

---
+++ LovelyUI/LovelyUI/Views/Welcome/WelcomeView.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeView.swift
+//  LovelyUI
+//
+//  Created by Debora Moura on 29/06/20.
+//  Copyright © 2020 Debora Moura. All rights reserved.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to Montaro UI Kit")
+            Text("The best UI Kit for your next health and fitness project!")
+            
+//            Button(action: {
+//            
+//            }) {
+//                
+//            }
+            Text("Already have account?")
+            Text("Sign in")
+        }
+    }
+}
