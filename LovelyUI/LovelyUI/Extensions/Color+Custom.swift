2e9558ddfa3126ff7b76f44befb0ef011d25f5d1
Débora Moura <debora.moura@ifood.com.br>
2021-08-12 17:12:20 -0300

    Create welcome view

--- LovelyUI/LovelyUI/Extensions/Color+Custom.swift
+++ LovelyUI/LovelyUI/Extensions/Color+Custom.swift
@@ -12,6 +12,7 @@ import SwiftUI
 extension Color {
     static let accentColor = Color(red: 110/255.0, green: 106/255.0, blue: 220/255.0)
     static let base = Color(red: 246/255.0, green: 246/255.0, blue: 250/255.0)
+    static let black = Color(red: 48/255.0, green: 51/255.0, blue: 66/255.0)
     static let cyan = Color(red: 147/255.0, green: 216/255.0, blue: 240/255.0)
     static let lightGray = Color(white: 200/255.0)
     static let darkGray = Color(white: 156/255.0)
@@ -21,6 +22,7 @@ extension Color {
 extension UIColor {
     static let accentColor = UIColor(red: 110/255.0, green: 106/255.0, blue: 220/255.0, alpha: 1.0)
     static let base = UIColor(red: 246/255.0, green: 246/255.0, blue: 250/255.0, alpha: 1.0)
+    static let black = UIColor(red: 48/255.0, green: 51/255.0, blue: 66/255.0, alpha: 1.0)
     static let cyan = UIColor(red: 147/255.0, green: 216/255.0, blue: 240/255.0, alpha: 1.0)
     static let lightGray = UIColor(white: 200/255.0, alpha: 1.0)
     static let darkGray = UIColor(white: 156/255.0, alpha: 1.0)
--- LovelyUI/LovelyUI/Views/Welcome/WelcomeView.swift
+++ LovelyUI/LovelyUI/Views/Welcome/WelcomeView.swift
@@ -11,16 +11,69 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         VStack {
-            Text("Welcome to Montaro UI Kit")
-            Text("The best UI Kit for your next health and fitness project!")
-            
-//            Button(action: {
-//            
-//            }) {
-//                
-//            }
-            Text("Already have account?")
-            Text("Sign in")
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, alignment: .center)
+                
+                Spacer()
+                
+                Text("Welcome to Montaro UI Kit")
+                    .font(Font.system(size: 30, weight: .semibold))
+                    .foregroundColor(Color.black)
+                    .frame(width: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 20)
+                            
+                Text("The best UI Kit for your next health and fitness project!")
+                    .font(Font.system(size: 17, weight: .regular))
+                    .frame(width: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Image("movement")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 385, alignment: .center)
+                    .clipped()
+                
+                Spacer()
+                
+                Button(action: {
+                    print("get started button")
+                }) {
+                    Text("Get Started")
+                        .fontWeight(.bold)
+                        .padding(.vertical, 15)
+                        .padding(.horizontal, 70)
+                        .background(Color.accentColor)
+                        .cornerRadius(15)
+                        .foregroundColor(Color.white)
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Already have account?")
+                        .font(Font.system(size: 17, weight: .regular))
+                        .foregroundColor(Color.darkGray)
+
+                    Button(action: {
+                        print("sign inbutton")
+                    }) {
+                        Text("Sign in")
+                            .font(Font.system(size: 17, weight: .bold))
+                            .foregroundColor(Color.accentColor)
+                    }
+                }
+                .frame(width: 300, alignment: .center)
+            }
+            .padding(.top, 70)
+            .padding(.bottom, 70)
         }
+        .background(Color.base)
+        .edgesIgnoringSafeArea(.all)
     }
 }
